95789c7e759084351f6e74947090ca13fb0f972f
Moomina Sereefa <130589221+moomina12@users.noreply.github.com>
2024-10-15 00:03:07 +0400

    Update exp6successfacemask.py

--- exp6successfacemask.py
+++ exp6successfacemask.py
@@ -7,6 +7,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
