62bbae1a8c3c0e2bb0f2a42b04185b725a237f0c
Moomina Sereefa <130589221+moomina12@users.noreply.github.com>
2024-10-14 12:48:34 +0400

    Update exp6successfacemask.py

--- exp6successfacemask.py
+++ exp6successfacemask.py
@@ -5,7 +5,7 @@
 
 
 import streamlit as st
-#import cv2
+import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
